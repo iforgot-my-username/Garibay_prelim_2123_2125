d9c0ee28b83c9a9c35a94ad175d706fa7277ff93
iforgot-my-username <garibay2018paul@gmail.com>
2021-10-01 15:46:36 +0800

    Added compute function

--- calculator.js
+++ calculator.js
@@ -2,48 +2,91 @@ let acBtn = document.querySelector("#clear-btn")
 let dotBtn = document.querySelector("#dot-btn");
 let zeroBtn = document.querySelector("#zero-btn");
 
+
 let input = "";
-let operator = '';
-let firstNumber = '';
+let operator = "";
+let firstNumber = "";
+let computed = false;
+
+
+function clear() {
+    input = "";
+    operator = "";
+    firstNumber = "";
+    computed = false;
+}
+
+function displayOnScreen(something) {
+    document.getElementById("screen").innerHTML = something;
+}
 
 acBtn.addEventListener("click", () => {
-    console.log("AC")
-    document.getElementById("screen").innerHTML = "0";
-    input = ""
+    console.log("AC");
+    displayOnScreen("0");
+    clear();
 })
 
 function refreshScreen() {
-    document.getElementById("screen").innerHTML = input;
+    displayOnScreen(input);
 }
 
 function getInput(str) {
-    input += str
-    refreshScreen()
+    input += str;
+    refreshScreen();
 }
 
 dotBtn.addEventListener("click", () => {
     const hasNoDot = !input.includes(".");
     if (hasNoDot) {
         if (input.length <= 0) {
-            getInput("0")
+            getInput("0");
         }
-        getInput(".")
+        getInput(".");
     }
 })
 
 zeroBtn.addEventListener("click", () => {
     if (input.length > 0) {
-        getInput("0")
-    }
+        getInput("0");
+    };
 })
 
 function getOperator(str) {
-    operator = str
+    operator = str;
     firstNumber = input;
-    input = '';
-    refreshScreen()
+    input = "";
+    refreshScreen();
 }
 
+function operate(operator) {
+    const a = Number(firstNumber);
+    const b = Number(input);
+
+    const add = () => a + b;
+
+    const subtract = () => a - b;
+
+    const divide = () => a / b;
+
+    const multiply = () => a * b
+
+    const operation = {
+        '+': add,
+        '-': subtract,
+        '/': divide,
+        '*': multiply
+    }
+
+    return operation[operator]();
+}
+
+function compute() {
+    if (!computed) {
+        displayOnScreen(operate(operator));
+        clear();
+        computed = true;
+    }
+}
 
 
 
