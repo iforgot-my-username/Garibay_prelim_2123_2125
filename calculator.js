980e6f313be839d4f07831e9529ee7bd0ca8d66c
iforgot-my-username <garibay2018paul@gmail.com>
2021-10-01 19:04:09 +0800

    remove onclick attribute in the html file. Added btn ids and value. Added document.querySelector.

--- calculator.js
+++ calculator.js
@@ -1,102 +1,131 @@
-let acBtn = document.querySelector("#clear-btn");
-let dotBtn = document.querySelector("#dot-btn");
-let zeroBtn = document.querySelector("#zero-btn");
+const pageLoaded = () => {
+    const acBtn = document.querySelector("#clear-btn");
+    const dotBtn = document.querySelector("#dot-btn");
+    const helloBtn = document.querySelector("#hello-btn");
+    const byeBtn = document.querySelector("bye-btn");
+    const zeroBtn = document.querySelector("#zero-btn");
+    const equalBtn = document.querySelector("#equal-btn");
 
 
-let input = "";
-let operator = "";
-let firstNumber = "";
-let computed = false;
+
+    const numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    const operators = ["plus", "minus", "times", "divide"]
 
 
-function displayOnScreen(something) {
-    document.getElementById("screen").innerHTML = something;
-}
-
-acBtn.addEventListener("click", () => {
-    console.log("AC");
-    displayOnScreen("0");
-    input = "";
-    operator = "";
-    firstNumber = "";
-    computed = false;
-})
-
-
-function refreshScreen() {
-    displayOnScreen(input);
-}
-
-function getInput(str) {
-
-    if (computed && firstNumber.length > 0) {
-        displayOnScreen("");
-        firstNumber = operator.length <= 0 ? "" : input;
-        computed = false;
-        input = str;
-    } else {
-        input += str;
-    }
-    refreshScreen()
-}
-
-
-dotBtn.addEventListener("click", () => {
-    const hasNoDot = !input.includes(".");
-
-    if (hasNoDot || computed) {
-        input.length <= 0 || computed ? getInput("0.") : getInput(".")
-    }
-})
-
-zeroBtn.addEventListener("click", () => {
-    if (input.length > 0) {
-        getInput("0");
-    };
-})
-
-function getOperator(str) {
-    if (!computed && firstNumber.length <= 0) {
-        operator = str
-        firstNumber = input;
-        input = '';
-        refreshScreen()
-    } else if (input.length > 0) {
-        compute()
-        operator = str
-    }
-}
-
-function operate(operator) {
-    const a = Number(firstNumber);
-    const b = Number(input);
-
-    const add = () => a + b;
-
-    const subtract = () => a - b;
-
-    const divide = () => a / b;
-
-    const multiply = () => a * b
-
-    const operation = {
-        '+': add,
-        '-': subtract,
-        '/': divide,
-        '*': multiply
+    const addOnclick = (button, dosomething) => {
+        const btn = document.querySelector(`#${button}-btn`)
+        btn.onclick = () => dosomething(btn.value)
     }
 
-    return operation[operator]();
-}
 
-function compute() {
-    if (!computed) {
-        input = String(operate(operator))
-        displayOnScreen(input);
+
+
+    let input = "";
+    let operator = "";
+    let firstNumber = "";
+    let computed = false;
+
+
+    function displayOnScreen(something) {
+        document.getElementById("screen").innerHTML = something;
+    }
+
+    acBtn.addEventListener("click", () => {
+        console.log("AC");
+        displayOnScreen("0");
+        input = "";
         operator = "";
-        computed = true;
+        firstNumber = "";
+        computed = false;
+    })
+
+
+    function refreshScreen() {
+        displayOnScreen(input);
     }
+
+    const getInput = (str) => {
+
+        if (computed && firstNumber.length > 0) {
+            displayOnScreen("");
+            firstNumber = operator.length <= 0 ? "" : input;
+            computed = false;
+            input = str;
+        } else {
+            input += str;
+        }
+        refreshScreen()
+    }
+
+
+    dotBtn.addEventListener("click", () => {
+        const hasNoDot = !input.includes(".");
+
+        if (hasNoDot || computed) {
+            input.length <= 0 || computed ? getInput("0.") : getInput(".")
+        }
+    })
+
+    zeroBtn.addEventListener("click", () => {
+        if (input.length > 0) {
+            getInput("0");
+        };
+    })
+
+    const getOperator = (str) => {
+        if (!computed && firstNumber.length <= 0) {
+            operator = str
+            firstNumber = input;
+            input = '';
+            refreshScreen()
+        } else if (input.length > 0) {
+            compute()
+            operator = str
+        }
+    }
+
+    function operate(operator) {
+        const a = Number(firstNumber);
+        const b = Number(input);
+
+        const add = () => a + b;
+
+        const subtract = () => a - b;
+
+        const divide = () => a / b;
+
+        const multiply = () => a * b
+
+        const operation = {
+            '+': add,
+            '-': subtract,
+            '/': divide,
+            '*': multiply
+        }
+
+        return operation[operator]();
+    }
+
+    const compute = () => {
+        if (!computed) {
+            input = String(operate(operator))
+            displayOnScreen(input);
+            operator = "";
+            computed = true;
+        }
+    }
+
+    equalBtn.addEventListener("click", compute)
+
+
+    const numbersOnclick = (button) => addOnclick(button, getInput)
+    const operatorsOnclick = (button) => addOnclick(button, getOperator)
+
+    numbers.forEach(numbersOnclick)
+    operators.forEach(operatorsOnclick)
 }
+
+pageLoaded();
 
 
 
